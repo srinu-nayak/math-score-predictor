0872559e261a3f8f53c559f82b6d4e1aed9eeba9
SRINU NAYAK <srinunayakk7@gmail.com>
2025-08-19 14:41:46 -0400

    preprocessing object is created

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from src.mlproject.exception import CustomException
 from src.mlproject.logger import logging
 from src.mlproject.components.data_ingestion import DataIngestion, DataIngestionConfig
+from src.mlproject.components.data_transformation import DataTransformation
+
 
 if __name__ == "__main__":
     try:
         raw_df, train_df, test_df = DataIngestion().get_sql_data()
         
-        print(train_df)
+        DataTransformation().started_data_transformation(train_df, test_df)
 
         
     except Exception as e:
---
+++ src/mlproject/components/data_transformation.py
@@ -0,0 +1,80 @@
+from src.mlproject.exception import CustomException
+from src.mlproject.logger import logging
+from dataclasses import dataclass
+import os
+import pandas as pd
+from sklearn.pipeline import  Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+
+@dataclass
+class DataTransformationConfig:
+    preprocessor_obj = os.path.join("artifacts", "preprocessor.pkl")
+
+class DataTransformation:
+    def __init__(self):
+        self.preprocessor_obj_config = DataTransformationConfig()
+
+    def transforming_data(self, input_feature_train_df):
+        try:
+
+            onehot_pipe = Pipeline(steps=[
+                ("onehot_encoder",OneHotEncoder(handle_unknown="ignore", drop="first")),
+                ("imputer", SimpleImputer(strategy="most_frequent")),
+            ])
+
+            ordinal_pipe = Pipeline(steps=[
+                ("ordinal_encoder", OrdinalEncoder(categories=[["high school", "some high school", "some college", "bachelor's degree","associate's degree", "master's degree"]])),
+                ("imputer", SimpleImputer(strategy="most_frequent")),
+            ])
+
+            numerical_pipe = Pipeline(steps=[
+                ("numerical_pipe", SimpleImputer(strategy="median")),
+                ("scaler", StandardScaler()),
+            ])
+
+            ct_preprocessor = ColumnTransformer(
+                transformers=[
+                    ("onehot_encoder", onehot_pipe, [0,1,3,4]),
+                    ("ordinal_encoder", ordinal_pipe, [2]),
+                    ("numerical_pipe", numerical_pipe, [5,6]),
+                ],
+                remainder="passthrough",
+            )
+
+            return ct_preprocessor
+
+        except Exception as e:
+            raise CustomException(e)
+
+    def started_data_transformation(self, train_df, test_df):
+        try:
+            logging.info(f"getting train_df, test_df for transformation from app file")
+
+            train_df = pd.read_csv(train_df)
+            test_df = pd.read_csv(test_df)
+
+            logging.info(f"seperating target feature from train_df")
+            input_feature_train_df = train_df.drop(columns=["math_score"])
+            target_feature_train_df = train_df["math_score"]
+
+            logging.info(f"seperating target feature from test_df")
+            input_feature_test_df = test_df.drop(columns=["math_score"])
+            target_feature_test_df = test_df["math_score"]
+
+            preprocessor_object = self.transforming_data(input_feature_train_df)
+
+
+            logging.info(f"preprocessor object created")
+            input_feature_train_arr = preprocessor_object.fit_transform(input_feature_train_df)
+            input_feature_test_arr = preprocessor_object.transform(input_feature_test_df)
+
+
+
+
+
+        except Exception as e:
+            raise CustomException(e)
+
